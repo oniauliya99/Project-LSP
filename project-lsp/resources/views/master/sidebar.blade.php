a76957e8447137b266b0fc090b967ffd6fd5a307
oniauliya99 <oniauliya99@gmail.com>
2021-10-23 21:05:00 +0700

    extract page

---
+++ project-lsp/resources/views/master/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="container-fluid">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div class="position-sticky pt-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <span data-feather="star"></span>
+                            Arsip
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <span data-feather="info"></span>
+                            About
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
